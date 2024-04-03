4ae90c710d4858657cb083014acbc680acef0299
salvagariglio <salvaglinkedin@gmail.com>
2024-04-03 16:34:32 -0300

    Food store created

---
+++ food-store/src/components/shared/Card.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {CardItem} from './CardItem'
+
+const Card = () => {
+    return (
+        <div>
+            <CardItem
+                img="comida.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="dish.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="comida.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="dish.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="comida.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="dish.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="comida.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="dish.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="comida.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+            <CardItem
+                img="dish.png"
+                description="Speacy seasoned seafood nodles"
+                price="2.29"
+                inventory="20"
+            />
+        </div>
+    )
+}
+
+export default Card
